4613fa7e34923b9387f57c50eda081327ca17020
jaikumar.i1310 <jaikumar.i1310@gmail.com>
2025-12-21 17:25:28 +0000

    idx - not seeing admin logging in - idx stating its fixed, unsure - lets see - 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,9 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import { initializeAuthDB } from "./lib/auth-db.ts";
 
-createRoot(document.getElementById("root")!).render(<App />);
+// Initialize the authentication database and default admin user
+initializeAuthDB().then(() => {
+  createRoot(document.getElementById("root")!).render(<App />);
+});
